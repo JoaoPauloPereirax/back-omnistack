72bc72d19edd29d9d6e855c3a6fdbd48b441796c
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-10 11:51:40 -0300

    Adding router to login system

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
-const { Router } = require('express');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 
 //ROUTES
 
+//LOGIN
+routes.post('/sessions', SessionController.create);
+
 //ONGS
-routes.get('/ongs', OngController.index)
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
 //PROFILE
