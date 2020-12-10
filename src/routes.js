cb0b0fafcbada010dd88cc73e0dae5750f612c02
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-10 09:49:30 -0300

    Adding router PROFILE

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,21 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const { Router } = require('express');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
 
 //ROUTES
 
+//ONGS
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create);
 
+//PROFILE
+routes.get('/profile', ProfileController.index);
+
+//INCIDENTS
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
